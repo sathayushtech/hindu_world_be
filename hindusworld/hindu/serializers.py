28bfa85fc7e38330f911273b8a59be0436e6a550
we-are-friends-123 <y19ec132@gmail.com>
2024-05-24 17:49:01 +0530

    2 commit

--- hindusworld/hindu/serializers.py
+++ hindusworld/hindu/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from .models import organization,Country
+from .utils import image_path_to_binary
 
 
 class OrgnisationSerializer(serializers.ModelSerializer):
+    org_images=serializers.SerializerMethodField()
+    def get_image_location(self, instance):
+            filename = instance.org_images
+            if filename:
+                format= image_path_to_binary(filename)
+                # print(format,"******************")
+                return format
+            return[]
     class Meta:
         model = organization
         fields = "__all__"
