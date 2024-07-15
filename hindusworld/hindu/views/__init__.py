d507f4f269523021af341a058db1fdf84dad2cf3
Mamathavanga24 <mamatha.sathayush@gmail.com>
2024-07-15 17:09:08 +0530

    district

--- hindusworld/hindu/views/__init__.py
+++ hindusworld/hindu/views/__init__.py
@@ -2,8 +2,8 @@ from .continent_view import continentsView
 from .country_view import CountryView,countries_by_Continent
 from .organization_view import OrgnizationView,GetItemByfield_InputView,AddOrgnization,GetOrgByStatus_Pending,GetOrgByStatus_Success,UpdateOrgStatus,GetItemByfields_InputViews,GetItemBystatefield_location,GetbyDistrictLocationOrganization,GetbyCountryLocationorganization,GetOrgbyroot_map
 # from .organization_view import OrgnizationView,AddOrgnization,GetIndianOrganizations,GetGlobalOrganizations,GetItemByfield_InputView,GetItemByfields_InputViews,GetOrgByStatus_Pending,GetOrgByStatus_Success,UpdateOrgStatus,GetOrgbyroot_map
-from .state_view import StateViews
-from .district_view import DistrictVIew
+from .state_view import StateViews,states_by_country
+from .district_view import DistrictVIew,districts_By_State
 # from .block_view import BlockView
 # from .village_view import VillageView
 # from .user_view import Registerview,LoginApiView,VerifyOtpView,ResendOtp,ForgotOtp,ResetPassword
--- hindusworld/hindu/views/district_view.py
+++ hindusworld/hindu/views/district_view.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
-from ..models import District
+from ..models import District,State
 from ..serializers import DistrictSerializer
 from rest_framework .response import Response
+from rest_framework import generics,status
 
 
 class DistrictVIew(viewsets.ModelViewSet):
@@ -34,3 +35,36 @@ class DistrictVIew(viewsets.ModelViewSet):
                 'message': 'Objects not found',
                 'status': 404
             })
+        
+
+
+
+
+
+class districts_By_State(generics.GenericAPIView):
+    serializer_class = DistrictSerializer
+
+    def get(self, request, state):
+        try:
+            # Fetch the districts using the provided state
+            districts = District.objects.filter(state=state)
+
+            serialized_data = DistrictSerializer(districts, many=True)
+            district_count = districts.count()
+
+            return Response({
+                "district_count": district_count,
+                "districts": serialized_data.data
+            }, status=status.HTTP_200_OK)
+        
+        except State.DoesNotExist:
+            return Response({
+                'message': 'State not found',
+                'status': 404
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as e:
+            return Response({
+                'message': str(e),
+                'status': 500
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
