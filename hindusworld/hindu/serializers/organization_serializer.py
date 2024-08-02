083b14930bf8d6404229d77a11f41dd0f2e7b943
SANDHYA1256 <sandhya.sathayush@gmail.com>
2024-08-02 17:26:32 +0530

    training

--- hindusworld/hindu/serializers/organization_serializer.py
+++ hindusworld/hindu/serializers/organization_serializer.py
@@ -34,12 +34,18 @@ class OrgnisationSerializer(serializers.ModelSerializer):
         model = Organization
         fields = '__all__'
     
-
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     if representation.get('org_images') in [None, 'null']:
+    #         representation['org_images'] = "image not found"
+    #     if representation.get('org_logo') in [None, 'null']:
+    #         representation['org_logo'] = "image not found"
+    #         return representation
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         # Fields to check for empty or null values
-        fields_to_check = ['org_images', 'org_logo', 'chairman', 'web_url', 'est_by','reg_id','est_date','location','organization_name','web_url','org_detail','geo_site','organization_members','sub_category_id','object_id','govt_id_proof']
+        fields_to_check = ['org_images', 'org_logo', 'chairman', 'web_url', 'est_by','country','reg_id','est_date','location','organization_name','web_url','org_detail','geo_site','organization_members','sub_category_id','object_id','govt_id_proof']
         for field in fields_to_check:
             if representation.get(field) in [None, '', 'null','-']:
                 representation[field] = "data not found"
